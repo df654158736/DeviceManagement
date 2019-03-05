5b60a3565ee4aca8ed434d166c202643a68e1820
df654158736 <df654158736@gmail.com>
2019-03-05 08:23:03 +0800

    v1

--- src/mainpage.js
+++ src/mainpage.js
@@ -1,21 +1,19 @@
 import React from "react";
-import { Row, Col } from "antd";
+import { Row, Col  } from "antd";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import NavLeft from "./components/NavLeft";
 import "./style/common.less";
-export default class Admin extends React.Component {
+export default class MainPage extends React.Component {
   render() {
     return (
       <Row className="container">
-        <Col span="4" className="nav-left">
+        <Col span='4' className="nav-left">
           <NavLeft />
         </Col>
         <Col span="20" className="main">
           <Header>Header</Header>
-          <Row className="content">
-          {this.props.children}
-          </Row>
+          <Row className="content">{this.props.children}</Row>
           <Footer>Footer</Footer>
         </Col>
       </Row>
